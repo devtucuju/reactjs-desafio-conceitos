ed465a5423f5c1e96fbc2378badccb69b6c99a3f
jsdeveloper <jros2.silva@gmail.com>
2020-04-11 00:02:12 -0300

    Implementação criar e remover repositorio

--- src/App.js
+++ src/App.js
@@ -1,28 +1,53 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
 import "./styles.css";
 
+import api from "./services/api";
+
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get("repositories").then((response) => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    try {
+      const response = await api.post("repositories", {
+        url: "https://github.com/euniorweb",
+        title: "Jose Silva",
+        techs: ["Node", "Express", "TypeScript", "React Native"],
+      });
+      setRepositories([...repositories, response.data]);
+    } catch (error) {
+      throw new Error("Error to save repository");
+    }
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    try {
+      await api.delete(`repositories/${id}`);
+      setRepositories(
+        repositories.filter((repository) => repository.id !== id)
+      );
+    } catch (error) {
+      throw new Error("Unexpected Error");
+    }
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repository) => (
+          <li key={`repository-${repository.id}`}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
-
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
